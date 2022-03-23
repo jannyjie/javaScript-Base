e86ad7010c69a2e365581f3e430a6f4fc983f1e3
jannyyijie <yuaog1331@gmail.com>
2022-03-23 23:04:10 +0800

    運算子

---
+++ JavaScripeBasic/運算式/運算子.js
@@ -0,0 +1,15 @@
+var num = 0;
+num = num + 2;
+num = num * 2;
+num = num / 2;
+num++;
+num--;
+
+num += 1 // 2
+num -= 1;
+num *= 1;
+num /= 1;
+num %= 3;
+
+console.log('num == 1 : ' + (num == 1)); // 3
+
