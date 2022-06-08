d1ad3d8efbf3773b6464edcff05d9afd69a4ce2a
unknown <yuag1331@gmail.com>
2022-06-08 19:44:14 +0800

    分頁運算邏輯

---
+++ JavaScripeBasic/小工具/分頁/複雜分業運算.js
@@ -0,0 +1,18 @@
+// 每次切換頁需要保持 10  
+let one = 11; // 開頭
+let two = 16; // 結尾
+let total = 16; // 總數 
+let num = 10; // 分頁數量
+let aa = [] // 運算 
+
+for (let i = 0; i < str; i++) {
+    aa.push(i + 1)
+}
+console.log(aa)
+if (one + 9 <= total) {
+    console.log(1)
+    console.log(aa.slice(one - 1, one + (num - 1)))
+} else if (one + 9 >= total) {
+    console.log(2)
+    console.log(aa.slice(total - num, total))
+}
